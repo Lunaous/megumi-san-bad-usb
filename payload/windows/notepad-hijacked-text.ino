bc062a9c2a73325d5e69a0d4bfaac397fc548abd
ゆきのシタ47 <bagusherp@gmail.com>
2017-10-08 00:22:27 +0700

    Create notepad-hijacked-text.ino

---
+++ payload/windows/notepad-hijacked-text.ino
@@ -0,0 +1,16 @@
+#include <NinjaKeyboard.h>
+
+EthicNinjaKeyboard en;
+
+void setup()
+{
+  //Inisialisasi program
+  en.init(1500);
+  en.winRUN("notepad");
+  en.echo (",,,,,,, ");
+  en.echo ("YOU HAVE BEEN HIJACKED BY YUKINOSHITA 47 :p ");
+}
+
+void loop() {
+
+}
