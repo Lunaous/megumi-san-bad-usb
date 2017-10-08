027a4765cb077f8ef8177d7641b7fd7806b7656a
ゆきのシタ47 <bagusherp@gmail.com>
2017-10-08 18:52:05 +0700

    Update notepad-hijacked-text.ino

--- payload/windows/notepad-hijacked-text.ino
+++ payload/windows/notepad-hijacked-text.ino
@@ -6,11 +6,13 @@ void setup()
 {
   //Inisialisasi program
   en.init(1500);
+  //Buka program notepad
   en.winRUN("notepad");
-  en.echo (",,,,,,, ");
-  en.echo ("YOU HAVE BEEN HIJACKED BY YUKINOSHITA 47 :p ");
+  //Tulis Hello world!
+  en.echo("YOU HAVE BEEN HIJACKED BY YUKINOSHITA 47 :p");
+  en.done();
 }
 
 void loop() {
-
+  
 }
